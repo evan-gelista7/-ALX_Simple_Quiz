d9d919179fb5dffd9d762c19944477224767bb2f
Wengelawit <wengelawitsamson@gmail.com>
2024-10-05 05:21:11 +0300

    Add index.html , style.css and quiz.js

--- quiz.js
+++ quiz.js
@@ -2,13 +2,13 @@ function checkAnswer() {
   const correctAnswer = "4";
   const userAnswer = document.querySelector('input[name="quiz"]:checked');
   if (userAnswer) {
-    const userAnswerValue = userAnswer.value;
+    const userAnswerValue = userAnswer.value; 
     const feedbackElement = document.getElementById("feedback");
     if (userAnswerValue === correctAnswer) {
-      feedbackElement.textContent = "Correct! Well done.";
+        feedbackElement.textContent = "Correct! Well done.";
     } else {
-      feedbackElement.textContent = "That's incorrect. Try again!";
+        feedbackElement.textContent = "That's incorrect. Try again!";
     }
-  }
+} 
 }
 document.getElementById("submit-answer").addEventListener("click", checkAnswer);
